2b39c6669d03daab1648abc03523f945bf0e6585
bhay1985 <bhay1985@users.noreply.github.com>
2017-09-18 22:40:32 -0400

    Creating basic structure for game
    Just creating classes and interfaces I think we'll need.  They're empty for now.  Also moved enum to it's own file where any enums can live.

--- ScriptRunner/Characters/Player.cs
+++ ScriptRunner/Characters/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScriptRunner.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,20 @@ namespace ScriptRunner
     {
         //Just populated these based on what I think it'd need.
         //  some basic implementation hooked up to these, feel free to change if needed.
-        public enum Color { Red, Green, Blue, Yellow, White};
-        public int Speed;
-        public Color CurrentColor;
+        //  Changing the name here because Color is a class somewhere in .net
+        
+        private int slicesPerSecond;
+        public int SlicesPerSecond { get { return slicesPerSecond; } }
+
+        private FirewallColors currentColor;
+        public FirewallColors CurrentColor { get { return currentColor; } }
 
         //single car only, don't know what to do for async psync yet
         public int SegmentPosition;
+
+        public Player()
+        {
+            
+        }
     }
 }
---
+++ ScriptRunner/Util/Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptRunner.Util
+{
+    //Not sure if we'll be using this in multiple files, so placing in it's own location for now.
+    public enum FirewallColors
+    {
+        White,
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
+
+    //Please help me come up with better names.
+    public enum Difficulty
+    {
+        ScriptKiddy,
+        Normal,
+        Hard
+    }
+}
